0b661feb6c674cc98eac208522c98c03648d9a1f
vatofk <gcidmaldonado4@gmail.com>
2025-02-17 09:49:29 -0300

    Agregado servicios nginx, loadbalancer, lamp

---
+++ LAMP-x86/var/www/html/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>DOCKER TEST</h1>
+    <h2>Haciendo Pruebas de docker</h2>
+    <h3>Este es un texto agregado despues de levantar el contenedor</h3>
+</body>
+</html>
